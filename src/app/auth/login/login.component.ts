1537dc0531c361919c898ea1326f86398bfd9db8
Hamza-Ziani-Dev <hamzaziani.dev@gmail.com>
2024-07-09 13:47:29 +0100

    'form builder and validation'

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, NgForm } from '@angular/forms';
 import { SharedModule } from '../../shared/shared/shared.module';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -15,6 +16,8 @@ export class LoginComponent {
   email: string = '';
   password: string = '';
 
+  constructor(private router: Router){}
+
 
 
   login(form: any) {
@@ -25,8 +28,10 @@ export class LoginComponent {
   }
 
   register() {
+    
     // Handle registration logic here
     console.log('Register button clicked');
+    this.router.navigate(['/auth/register']);
   }
 
 }
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { SharedModule } from '../../shared/shared/shared.module';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -9,5 +10,39 @@ import { SharedModule } from '../../shared/shared/shared.module';
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+
+  form : FormGroup = new FormGroup({});
+
+
+  
+constructor(private formBuilder: FormBuilder) {}
+
+ngOnInit(): void {
+  this.formModel();
+}
+  // form : FormGroup = new FormGroup({
+  //   firstName: new FormControl(''),
+  //   lastName: new FormControl(''),
+  //   email: new FormControl(''),
+  //   password: new FormControl(''),
+  //   confirmPassword: new FormControl(''),
+  // });
+
+
+
+
+
+
+private formModel() : void{
+  this.form = this.formBuilder.group({
+    firstName: ['',Validators.required, Validators.minLength(3)],
+    lastName: ['',Validators.required, Validators.minLength(3)],
+    email: ['',Validators.required, Validators.email],
+    password: ['',Validators.required, Validators.minLength(6)],
+    confirmPassword: ['',Validators.required, Validators.minLength(6)],
+  })
+}
+
+
 
 }
